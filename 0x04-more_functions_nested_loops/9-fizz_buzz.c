714c365cd51fcd97751b042367e70e4a04c06ebb
Anyamgu <anyangudelight@gmail.com>
2023-09-15 15:47:11 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Entry point
@@ -7,7 +6,7 @@
  * Fizz for multiple of 3 , Buzz for multiple of 5
  * and FizzBuzz for multiples of both
  *
- * Reurn: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
