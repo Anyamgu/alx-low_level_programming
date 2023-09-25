38a1c0d87265468f83048b6bd83c333549c3309e
Anyamgu <anyangudelight@gmail.com>
2023-09-25 04:12:38 -0700

    5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string to be searched
+ * @needle: substring to be located
+ *
+ * Return: a pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			char *h = haystack;
+			char *n = needle;
+
+			while (*n != '\0' && *h == *n)
+			{
+				h++;
+				n++;
+			}
+			if (*n == '\0')
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+
+	return ('\0');
+}
