1c2ce895af08467e61d0b8ede74a6b2578d2bfa7
Anyamgu <anyangudelight@gmail.com>
2023-10-09 09:56:56 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print the name of the source file and a newline
+ *
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
