83eb0bbed2513b931c4bd81a4eddb737d3982ab0
Anyamgu <anyangudelight@gmail.com>
2023-10-12 10:51:55 -0700

    3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
- * print_all - Prints a variable number of arguments based on the format
- * @format: The format string containing format specifiers
- * @...: A variable number of arguments
+ * print_all - Prints a variable number of arguments based on the format.
+ * @format: The format string containing format specifiers.
+ * @...: A variable number of arguments.
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list ap;
+	char *sep = "";
 	int i = 0;
 	char *str;
 
@@ -17,34 +18,28 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (i > 0)
+		switch (format[i])
 		{
-			printf(", ");
+			case 'c':
+				printf("%s%c", sep, va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		if (format[i] == 'c')
-		{
-			printf("%c", va_arg(ap, int));
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%d", va_arg(ap, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%f", va_arg(ap, double));
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(ap, char *);
-			if (str == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", str);
-			}
-		}
+		sep = ", ";
 		i++;
 	}
 	va_end(ap);
