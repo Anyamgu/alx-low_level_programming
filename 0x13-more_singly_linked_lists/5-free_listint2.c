c98bbc83e81ce2fbb5a4c88d149ccc742d9a9321
Anyamgu <anyangudelight@gmail.com>
2023-10-25 00:59:26 -0700

    5-free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,10 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
-		current = *head;
-		*head = (*head)->next;
-		free(current);
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
+	*head = current;
 }
